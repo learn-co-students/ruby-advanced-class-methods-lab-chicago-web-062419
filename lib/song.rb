d2cd7b994ef8e3d0c2634b04eb9c54087e5fac1e
Amanda Treutler <amandathedev@gmail.com>
2019-07-01 21:31:08 -0500

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -1,5 +1,6 @@
+require 'pry'
 class Song
-  attr_accessor :name, :artist_name
+  attr_accessor :name, :artist_name, :save
   @@all = []
 
   def self.all
@@ -8,6 +9,66 @@ class Song
 
   def save
     self.class.all << self
+  end
+
+  def self.create
+    song = self.new
+    # Song.new == self
+    song.save
+    return song
+  end
+
+  def self.new_by_name(name)
+    song = self.new
+    # binding.pry
+    song.name = name
+    song
+    # binding.pry
+    # song.name
+  end
+
+  def self.create_by_name(name)
+    song = self.create
+    # binding.pry
+    song.name = name
+    song
+  end
+
+  def self.find_by_name(song)
+    self.all.find {|track| track.name == song}
+  end
+  
+  def self.find_or_create_by_name(name)
+    self.find_by_name(name) || self.create_by_name(name)
+  end
+  
+  def self.alphabetical
+    self.all.sort_by{|song| song.name}
+    # binding.pry
+  end
+
+  def self.new_from_filename(file)
+    file_array = file.split(" - ")
+    # binding.pry
+    song = self.new
+    song.artist_name = file_array[0]
+    song.name = file_array[1].chomp('.mp3')
+    song
+    # binding.pry
+  end
+
+  def self.create_from_filename(file)
+    file_array = file.split(" - ")
+    # binding.pry
+    song = self.create
+    # song_name = song.split("")
+    song.artist_name = file_array[0]
+    song.name = file_array[1].chomp('.mp3')
+    song
+  end
+
+  def self.destroy_all
+    @@all = []
   end
 
 end
